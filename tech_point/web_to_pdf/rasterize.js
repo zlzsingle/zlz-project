fe5f537a03aa58e5745ab52fdb120783b7debd71
zlz-notebook <774320433@qq.com>
2018-08-01 20:09:28 +0800

    refactor(tech_point/): 修改phantomJs

--- tech_point/web_to_pdf/rasterize.js
+++ tech_point/web_to_pdf/rasterize.js
@@ -25,7 +25,7 @@ if (system.args.length < 3 || system.args.length > 5) {
             window.setTimeout(function () {
                 page.render(output);
                 phantom.exit();
-            });
+            },3000);
         }
     });
 }
