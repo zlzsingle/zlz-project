e9f935ca2ac2268b48e5b54a26dad97def6e2eff
zlz-notebook <774320433@qq.com>
2018-08-03 16:59:25 +0800

    docs(commitizen.js): 修改描述

--- nodejs/commitizen.js
+++ nodejs/commitizen.js
@@ -1,11 +1,14 @@
 /*
-    feat：新功能（feature）
-    fix：修补bug
-    docs：文档（documentation）
-    style： 格式（不影响代码运行的变动）
-    refactor：重构（即不是新增功能，也不是修改bug的代码变动）
-    test：增加测试
-    chore：构建过程或辅助工具的变动
+    . feat：新功能(feature)
+    . fix :修复bug
+    . doc : 文档改变(documentation)
+    . style : 代码格式改变(不影响代码运行的变动)
+    . refactor :某个已有功能重构(即不是新增功能，也不是修改bug的代码变动)
+    . perf :性能优化
+    . test :增加测试
+    . build :改变了build工具 如 grunt换成了 npm
+    . revert: 撤销上一次的 commit
+    . chore: 构建过程或辅助工具的变动
 
     commitizen init cz-conventional-changelog --save --save-exact  //初始化commitzen
 */
