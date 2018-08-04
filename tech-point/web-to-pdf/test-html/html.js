d59c53073f33160731ce6d6f70190b5fa1f7767b
zlz-notebook <774320433@qq.com>
2018-08-04 16:02:51 +0800

    refactor(tech-point): 刪除tech-point

--- tech-point/web-to-pdf/test-html/html.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    var div = document.createElement("div");
-    div.innerHTML = "我是div";
-    document.body.appendChild(div);
-})();
--- tech-point/webpage.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function () {
-    console.error(1);
-    phantom.exit();
-},5000);
