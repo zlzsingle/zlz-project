feff543237cdf7df2195a2e0999a9ad3ac07ea0b
zlz-notebook <774320433@qq.com>
2018-08-14 00:46:51 +0800

    feat:

--- file/api.js
+++ file/api.js
@@ -79,3 +79,217 @@
 exports.getUserAccounts = async ctx =>{
 
 };
+
+/**
+ * @api {get}  /api/v1/social/attention/state/with/two/user getAttentionFansOfUser
+ * @apiDescription 获取两个用户相互关注的状态  (现有接口,无需改动)
+ *                 <br/><br/>开发者：[庄伦植](mailto:zhuanglunzhi@followme-inc.com)
+ * @apiGroup    Account
+ *
+ * @apiExample Example usage:
+ *      GET /api/v1/social/attention/state/with/two/user?userId=100&toUserId=120
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "code": "SUCCESS",
+ *          "data": {
+ *              "AttentionHe": false,
+ *              "AttentionMe": false
+ *          }
+ *      }
+ *
+ * @apiParam    (Parameter: query)  {number}   [userId]     主用户id
+ * @apiParam    (Parameter: query)  {number}   [toUserId]   次用户id
+ *
+ * @apiSuccess  {string}    code                            SUCCESS
+ * @apiSuccess  {object}    data                            数据
+ * @apiSuccess  {string}    data.AttentionHe                当前用户是否关注目标用户
+ * @apiSuccess  {string}    data.AttentionMe                目标用户是否关注当前用户
+ *
+ */
+exports.getAttentionFansOfUser = async ctx =>{
+
+};
+
+/**
+ * @api {get}  /api/v2/trader/user/:userId/trader-symbols getTraderSymbol
+ * @apiDescription 获取用户常交易的品种信息  (新增接口)
+ *                 <br/><br/>开发者：[庄伦植](mailto:zhuanglunzhi@followme-inc.com)
+ * @apiGroup    Account
+ *
+ * @apiExample Example usage:
+ *      GET /api/v2/trader/user/9999/trader-symbols?count=3
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "code": "SUCCESS",
+ *          "data": {
+ *               symbols : [
+ *                      {
+ *                          SymbolId : 11,
+ *                          SymbolCNName:"澳元/纽元",
+ *                          SymbolName:"AUD/NZD",
+ *                          StandardLots : 1.98,
+ *                          Money : 100.25,
+ *                          TraderAccount　：　[
+ *                              {
+ *                                 "UserId": 151537,
+ *                                 "UserType": 1,
+ *                                 "NickName": "刘庚家",
+ *                                 "Gender": 0,
+ *                                 "BrokerId": 104,
+ *                                 "Account" : "888888",
+ *                                 "AccountIndex": 2,
+ *                                 "CreateTime": "1512906169",
+ *                                 "Avatar": "https://static.followme.com/avatar/avatar-151537-2018-07-10-23-40-10-757331.png",
+ *                              }
+ *                          ]
+ *                      }
+ *               ]
+ *          }
+ *      }
+ *
+ * @apiParam    (Parameter: params)  {number}   [userId]            用户id
+ * @apiParam    (Parameter: query)  {number}   [count]              要获取的品种总数,默认3.取值1~10
+ *
+ * @apiSuccess  {string}    code                                    SUCCESS
+ * @apiSuccess  {object}    data                                    数据
+ * @apiSuccess  {string}    data.symbols                            品种列表
+ * @apiSuccess  {string}    data.symbols.SymbolId                   品种Id
+ * @apiSuccess  {string}    data.symbols.SymbolCNName               品种中文名称
+ * @apiSuccess  {string}    data.symbols.SymbolName                 品种名称
+ * @apiSuccess  {string}    data.symbols.StandardLots               累计标准手
+ * @apiSuccess  {string}    data.symbols.Money                      累计收益
+ * @apiSuccess  {string}    data.symbols.TraderAccount　            该品种其他优秀交易员
+ * @apiSuccess  {string}    data.symbols.TraderAccount.UserId       用户id
+ * @apiSuccess  {string}    data.symbols.TraderAccount.UserType     用户类型
+ * @apiSuccess  {string}    data.symbols.TraderAccount.NickName     昵称
+ * @apiSuccess  {string}    data.symbols.TraderAccount.Gender       性别
+ * @apiSuccess  {string}    data.symbols.TraderAccount.BrokerId     经纪商id
+ * @apiSuccess  {string}    data.symbols.TraderAccount.Account      优秀交易员的交易账号
+ * @apiSuccess  {string}    data.symbols.TraderAccount.AccountIndex 账号索引
+ * @apiSuccess  {string}    data.symbols.TraderAccount.CreateTime   注册时间
+ * @apiSuccess  {string}    data.symbols.TraderAccount.Avatar       交易员头像地址
+ *
+ */
+exports.getAttentionFansOfUser = async ctx =>{
+
+};
+
+/**
+ * @api {get}  /api/v2/trader/user/attentions/broker getAttentionBrokers
+ * @apiDescription 获取关注的经纪商  (新增接口)
+ *                 <br/><br/>开发者：[庄伦植](mailto:zhuanglunzhi@followme-inc.com)
+ * @apiGroup    User
+ *
+ * @apiExample Example usage:
+ *      GET /api/v2/trader/user/attentions/broker?userId=999
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "code": "SUCCESS",
+ *          "data": {
+ *                 brokers : [
+ *                            {
+ *                                BrokerID: 1,
+ *                                Name: "pico",
+ *                                FullName: "RH晋峰环球国际",
+ *                                Introduce: "晋峰金融集团有限公司(“晋峰金融集团”)为投资控股公司，根据英属维尔京群岛法例注册成立的合法机构。晋峰金融集团由两位具广泛人脉、丰富金融投资经验及知识的独立投资者创立。主要透过其全资拥有的子公司晋峰资本投资管理有限公司(“晋峰资本”)及其旗下子公司进行多元化主流业务，包括：期货﹑证劵经纪业务﹑借贷人业务﹑和资产管理业务。",
+ *                               Website: "http://www.fxrhs.com/",
+ *                               OpenAccountURL: "https://passport.fxrhs.com/Register/MailRegister",
+ *                               IconURL: "https://cdn.followme.com/images/broker/web/1/1_15_15.png",
+ *                               BigIconURL: "https://cdn.followme.com/images/broker/web/1/1_50_50.png",
+ *                               MinLots: 0.1,
+ *                               Flag: 1227,
+ *                               IsActive: true,
+ *                               DepositeURL: "https://passport.fxrhs.com/Account/Login",
+ *                               ShortName: "",
+ *                               Sort: 0
+ *                           }
+ *                   ]
+ *          }
+ *      }
+ *
+ * @apiParam    (Parameter: params)  {number}   [userId]            用户id
+ *
+ * @apiSuccess  {string}    code                                    SUCCESS
+ * @apiSuccess  {object}    data                                    数据
+ * @apiSuccess  {string}    data.brokers                            品种列表
+ * @apiSuccess  {string}    data.brokers.BrokerID
+ * @apiSuccess  {string}    data.brokers.Name
+ * @apiSuccess  {string}    data.brokers.FullName
+ * @apiSuccess  {string}    data.brokers.Introduce
+ * @apiSuccess  {string}    data.brokers.Website
+ * @apiSuccess  {string}    data.brokers.OpenAccountURL
+ * @apiSuccess  {string}    data.brokers.IconURL
+ * @apiSuccess  {string}    data.brokers.BigIconURL
+ * @apiSuccess  {string}    data.brokers.MinLots
+ * @apiSuccess  {string}    data.brokers.Flag
+ * @apiSuccess  {string}    data.brokers.IsActive
+ * @apiSuccess  {string}    data.brokers.DepositeURL
+ * @apiSuccess  {string}    data.brokers.ShortName
+ * @apiSuccess  {string}    data.brokers.Sort
+ *
+ */
+exports.getAttentionBrokers = async ctx =>{
+
+};
+
+/**
+ * @api {get}  /api/v2/social/personal/:userId/album getUserAlbum
+ * @apiDescription 获取相册  (新增接口)
+ *                 <br/><br/>开发者：[庄伦植](mailto:zhuanglunzhi@followme-inc.com)
+ * @apiGroup    User
+ *
+ * @apiExample Example usage:
+ *      GET /api/v2/social/personal/151537/album?count=6&beginTime=9999999
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "code": "SUCCESS",
+ *          "data": {
+ *               album : [
+ *                   {
+ *                       Id : 999,
+ *                       BlogBody : "",
+ *                       keywords : "",　
+ *                       CreateTime　: "",
+ *                       CreateTimeDesc : "",
+ *                       Files : [
+ *                           {
+ *                               ContentType:"png",
+ *                               ObjectId:20471983,
+ *                               Seq:47858,
+ *                               Url:"https://static.followme.com/social/201807/d59d350896ac48b19f3bef379f1cb9d2.png"
+ *                           }
+ *                       ]
+ *                   }
+ *               ]
+ *          }
+ *      }
+ *
+ * @apiParam    (Parameter: params)  {number}   [count]             获取的相册数,默认6条.取值 1~20
+ * @apiParam    (Parameter: query)  {number}   [beginTime]          从那个时间点开始获取,不传默认所有
+ *
+ * @apiSuccess  {string}    code                                    SUCCESS
+ * @apiSuccess  {object}    data                                    数据
+ * @apiSuccess  {string}    data.album                              相册
+ * @apiSuccess  {string}    data.album.Id                           微博id
+ * @apiSuccess  {string}    data.album.BlogBody                     微博正文
+ * @apiSuccess  {string}    data.album.keywords                     关键词
+ * @apiSuccess  {string}    data.album.CreateTime　                 创建时间
+ * @apiSuccess  {string}    data.album.CreateTimeDesc               创建时间描述
+ * @apiSuccess  {string}    data.album.Files                        图片
+ * @apiSuccess  {string}    data.album.Files.ContentType            文件类型
+ * @apiSuccess  {string}    data.album.Files.ObjectId               文件id
+ * @apiSuccess  {string}    data.album.Files.Seq                    文件大小
+ * @apiSuccess  {string}    data.album.Files.Url                    文件地址
+ *
+ */
+exports.getUserAlbum = async ctx =>{
+
+};
