2e3194d177eeb1329038561696e5c1583ef8298e
zlz-notebook <774320433@qq.com>
2018-08-26 21:10:22 +0800

    docs: 删除book

--- nodejs/moment.js
+++ nodejs/moment.js
@@ -7,3 +7,12 @@ moment().valueOf(); // 时间戳
 moment().unix(); // unix时间戳
 
 moment.unix(1530374400).format("MM/DD/YYYY"); //指定时间戳获取格式
+
+moment().format('L');    // 2018-08-26
+moment().format('l');    // 2018-08-26
+moment().format('LL');   // 2018年8月26日
+moment().format('ll');   // 2018年8月26日
+moment().format('LLL');  // 2018年8月26日晚上6点25分
+moment().format('lll');  // 2018年8月26日晚上6点25分
+moment().format('LLLL'); // 2018年8月26日星期日晚上6点25分
+moment().format('llll'); // 2018年8月26日星期日晚上6点25分
