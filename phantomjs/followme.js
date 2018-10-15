71c347a1327dfcc4f8faa3f0a5add8f4c49d95de
zhuanglunzhi <774320433@qq.com>
2018-10-15 23:52:42 +0800

    add files

---
+++ phantomjs/followme.js
@@ -0,0 +1,21 @@
+var page = require('webpage').create();
+var count = 1;
+
+function getPage() {
+    
+    page.open('https://www.followme.com/api/v1/account/users/193431', function () {
+        console.error(2);
+        setTimeout(function () {
+            console.error('count : ', count);
+            if (count > 1000) {
+                phantom.exit();
+            } else {
+                count = count + 1;
+              getPage();
+            }
+        }, 200);
+    });
+}
+
+console.error(1);
+getPage();
