2b56501dfc80aeb57407fbbcf31ea8995b47ec74
zhuanglunzhi <774320433@qq.com>
2018-11-28 16:18:21 +0800

    add nodejs files

--- shell/test.sh
+++ shell/test.sh
@@ -26,3 +26,92 @@
 
 #string='my name is job'
 #echo `expr index "${string}" job`
+
+#array_name=(1 2 3 4 5)
+#
+#length=${#array_name[@]}
+#
+#echo ${length}
+#
+#length=${#array_name[*]}
+#
+#echo ${length}
+#
+#lengthn=${#array_name[n]}
+#
+#echo ${length}
+
+#echo "Shell 传递参数实例！";
+#echo "执行的文件名：$0";
+#echo "第一个参数为：$1";
+#echo "第二个参数为：$2";
+#echo "第三个参数为：$3";
+
+# ./test.sh 1 2 3
+
+
+#echo "-- \$* 演示 ---"
+#for i in "$*"; do
+#    echo $i
+#done
+#
+#echo "-- \$@ 演示 ---"
+#for i in "$@"; do
+#    echo $i
+#done
+
+
+a=10
+b=20
+
+#echo `expr ${a} + ${b}`
+#echo `expr ${a} - ${b}`
+#echo `expr ${a} \* ${b}`
+#echo `expr ${a} / ${b}`
+#echo `expr ${a} % ${b}`
+
+#if [ ${a} == ${b} ]
+#then
+#   echo "a 等于 b"
+#fi
+#if [ ${a} != ${b} ]
+#then
+#   echo "a 不等于 b"
+#fi
+
+#if [ ${a} -eq ${b} ]
+#then
+#    echo 'a 等于 b'
+#fi
+#
+#if [ ${a} -ne ${b} ]
+#then
+#    echo 'a 不等于 b'
+#fi
+#
+#if [ ${a} -gt ${b} ]
+#then
+#    echo 'a 大于 b'
+#fi
+#
+#if [ ${a} -lt ${b} ]
+#then
+#    echo 'a 小于 b'
+#fi
+#
+#if [ ${a} -ge ${b} ]
+#then
+#    echo 'a 大于等于 b'
+#fi
+#
+#if [ ${a} -le ${b} ]
+#then
+#    echo 'a 小于等于 b'
+#fi
+
+if [ ${a} != ${b} ]
+then
+    echo ' a != b'
+else
+    echo ' a == b'
+fi
