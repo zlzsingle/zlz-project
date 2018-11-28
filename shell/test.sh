d6832ace50fea2f7916549775d4e040f2e10748e
zhuanglunzhi <774320433@qq.com>
2018-11-28 15:34:22 +0800

    add nodejs files

---
+++ shell/test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#for file in `ls /home/zlz`; do
+#    echo "${file}"
+#done
+
+#name=ZS
+#echo $name
+#readonly name
+#name=LS
+
+#your_name="runoob"
+## 使用双引号拼接
+#greeting="hello, "$your_name" !"
+#greeting_1="hello, ${your_name} !"
+#echo $greeting
+#echo $greeting_1
+
+
+# 使用单引号拼接
+#greeting_2='hello, '$your_name' !'
+#greeting_3='hello, ${your_name} !'
+#echo $greeting_2
+#echo $greeting_3
+
+
+#string='my name is job'
+#echo `expr index "${string}" job`
