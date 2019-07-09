7361868be1b177d218170f4e7ad90fcf0356db2c
zhuanglunzhi <zhuanglunzhi@followme.cn>
2019-07-09 10:55:43 +0800

    add or update files

--- nodejs/pm2.js
+++ nodejs/pm2.js
@@ -11,3 +11,49 @@
 // https://blog.csdn.net/gong1422425666/article/details/74943073  pm2 deploy 部署
 
 // https://blog.csdn.net/cs380637384/article/details/82682799
+
+/*
+    pm2自动化部署
+        1.确保服务器可以正常clone项目，将ssh密钥加入gitlab或github.
+        2.在本地配置服务器上的ssh配置，修改.ssh/config文件
+            Host server-name
+            HostName             192.168.xx.xx
+            User                 suser
+            PubkeyAuthentication yes
+
+        3.执行ssh-copy-id server-name
+
+        4.在项目里创建ecosystem.json文件
+            {
+                "apps" : [
+                    {
+                        "name" : "project-name",
+                        "script" : "dist/main.js",
+                        "max_memory_restart" : "800M",
+                        "env" : {
+                            "NODE_ENV" : "dev"
+                        },
+                        "env_prod" : {
+                            "NODE_ENV" : "production"
+                        }
+                    }
+                ],
+                "deploy" : {
+                    "dev" : {
+                        "user" : "suser",
+                        "host" : "192.168.xx.xx",
+                        "ref" : "origin/master",
+                        "repo" : "git@gitlab.xxxx.xxx:groupName/project.git",
+                        "path" : "~/www/project",
+                        "post-deploy" : "~/.nvm/nvm.sh && nvm use 10.9 && yarn && npm run test && pm2 start ecosystem.json --env dev"
+                    }
+                }
+            }
+
+        5.登录server-name服务器,创建项目目录. mkdir -p ~/www/project
+
+        6.回到本地,进入项目目录执行
+            pm2 deploy ecosystem.json dev setup
+            pm2 deploy ecosystem.json dev --force
+
+*/
