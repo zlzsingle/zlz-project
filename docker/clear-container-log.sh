938aed7939dd0c4d676bc8c9856456fa64591f33
zhuanglunzhi <zhuanglunzhi@followme.cn>
2019-09-04 16:51:25 +0800

    add or update files

--- docker/clear-container-log.sh
+++ docker/clear-container-log.sh
@@ -1,13 +1,13 @@
-#!/bin/sh 
+#!/usr/bin/env bash
 
-echo "======== start clean docker containers logs ========"  
-  
-logs=$(find /var/lib/docker/containers/ -name *-json.log)  
-  
-for log in $logs  
-        do  
-                echo "clean logs : $log"  
-                cat /dev/null > $log  
-        done  
+echo "======== start clean docker containers logs ========"
 
-echo "======== end clean docker containers logs ========"  
+logs=$(find /var/lib/docker/containers/ -name *-json.log)
+
+for log in $logs
+    do
+            echo "clean logs : $log"
+            cat /dev/null > $log
+    done
+
+echo "======== end clean docker containers logs ========"
