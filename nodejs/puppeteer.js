79ebc60ef31d0c021953d0b1760f4a304d2194a7
zhuanglunzhi <zhuanglunzhi@followme.cn>
2019-12-28 14:24:34 +0800

    add or update files

--- nodejs/puppeteer.js
+++ nodejs/puppeteer.js
@@ -1,1 +1,25 @@
- // puppeteer 无界面浏览器
+// puppeteer 无界面浏览器
+
+// https://pptr.dev/
+
+(async () => {
+
+    const puppeteer = require('puppeteer');
+
+    // 设备大全
+    const devices = require('puppeteer/DeviceDescriptors');
+
+    // 打开浏览器
+    const browser = await puppeteer.launch({headless: true});
+
+    // 新开一个页面
+    const page = await browser.newPage();
+
+    // 模拟 iphoneX 访问
+    await page.emulate(devices['iPhone X']);
+
+    // 新开一个页面
+    await page.goto('http://www.baidu.com');
+
+
+})();
