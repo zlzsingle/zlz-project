840495efeb0a161c583c32b65dfda02aa10629c3
zlzlife <zlzlife888@gmail.com>
2020-09-07 11:20:46 +0800

    add or update files

--- nodejs/yarn.js
+++ nodejs/yarn.js
@@ -6,3 +6,12 @@
 // yarn config set chromedriver_cdnurl https://cdn.npm.taobao.org/dist/chromedriver -g
 // yarn config set operadriver_cdnurl https://cdn.npm.taobao.org/dist/operadriver -g
 // yarn config set fse_binary_host_mirror https://npm.taobao.org/mirrors/fsevents -g
+
+// yarn config delete registry
+// yarn config delete disturl
+// yarn config delete electron_mirror
+// yarn config delete sass_binary_site
+// yarn config delete phantomjs_cdnurl
+// yarn config delete chromedriver_cdnurl
+// yarn config delete operadriver_cdnurl
+// yarn config delete fse_binary_host_mirror
